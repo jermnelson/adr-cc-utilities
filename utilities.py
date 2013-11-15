20cc224fde6e074c6584c52b2cc0dcc01eac0bdd
Jeremy Nelson <jermnelson.gmail.com>
2013-11-15 13:04:09 -0700

    PID Mover now displaying and sending to Fedora, still needs work

--- utilities.py
+++ utilities.py
@@ -12,7 +12,7 @@ __license__ = 'Apache License, Version 2.0'
 __copyright__ = '(c) 2013 by Jeremy Nelson'
 
 from flask import Flask, request, session, redirect, url_for
-from flask import abort, render_template, flash
+from flask import abort, jsonify, render_template, flash
 #! Hack to load Fedora commons Flask Extension for test and usage
 import sys
 sys.path.append("C:\\Users\\jernelson\\Development\\flask-fedora")
@@ -24,7 +24,38 @@ fedora = FedoraCommons(app)
 
 @app.route('/')
 def default():
-    return "ADR-CC Utilities, fedora={0}".format(fedora.__doc__)
+    "Default view of utilities"
+    return render_template('index.html')
+                           
+
+@app.route("/pid-mover", methods=['POST', 'GET'])
+def pid_mover():
+    "PID Mover view"
+    # Should be an AJAX call
+    if request.method == 'POST':
+        collection_pid = request.form['collection_pid']
+        source_pid = request.form['source_pid']
+        if not fedora.move(source_pid, collection_pid):
+            result = "error"
+            msg = "Could not move {0} to {1} collection".format(
+                source_pid,
+                collection_pid)
+        else:
+            result = "ok",
+            msg = "{0} moved to {1} collection".format(
+                source_pid,
+                collection_pid)
+        return jsonify(**{"result": result,
+                          "message": msg})
+    return render_template("pid-mover.html",
+                           section='mover')
+
+@app.route("/batch-template-add",)
+def batch_template_add():
+    """View for adding one or more Fedora objects to the repository by using a
+    template."""
+    return render_template('batch-template-add.html',
+                           section='batch-add')
 
 if __name__ == '__main__':
     host = '0.0.0.0'
