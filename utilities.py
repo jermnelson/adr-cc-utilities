41eb29852cbb4ada1c9ce8f810d15dba3e8378b4
Jeremy Nelson <jermnelson.gmail.com>
2013-11-15 09:24:06 -0700

    adding Bootstrap base template

--- utilities.py
+++ utilities.py
@@ -13,14 +13,27 @@ __copyright__ = '(c) 2013 by Jeremy Nelson'
 
 from flask import Flask, request, session, redirect, url_for
 from flask import abort, render_template, flash
+#! Hack to load Fedora commons Flask Extension for test and usage
+import sys
+sys.path.append("C:\\Users\\jernelson\\Development\\flask-fedora")
 from flask_fedora_commons import FedoraCommons
 
-app = Flask(__app__)
+app = Flask(__name__)
 app.config.from_pyfile('fedora.cfg')
 fedora = FedoraCommons(app)
+
+@app.route('/')
+def default():
+    return "ADR-CC Utilities, fedora={0}".format(fedora.__doc__)
 
 if __name__ == '__main__':
     host = '0.0.0.0'
     port = 8003
-    app.run(host=host,
-            port=port)
+    mode = 'dev'
+    if mode == 'dev':
+        app.run(host=host,
+                port=port,
+                debug=True)
+    else:
+        app.run(host=host,
+                port=port)
